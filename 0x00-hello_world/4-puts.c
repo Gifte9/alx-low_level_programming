4bd440ad94229126faa7d7e6cc83e55af631c5b2
Gifte9 <emekegift14@gmail.com>
2023-08-12 03:03:47 -0700

    5-printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 /**
  * Description: A C programming for building puzzlee.
  */
-int main(void)
-{puts("\"Programming is like building a multilingual puzzle");return(0);}
+int main(void){
+puts("\"Programming is like building a multilingual puzzle");return(0);}
 
 
